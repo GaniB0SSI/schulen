086e6d2134d0c0f7095df23d0ac226ef16f83397
Gani Hasmegaj <ganhas20@htl-shkoder.com>
2025-02-26 11:46:02 +0100

    new changes

--- src/lib/database/mysql.js
+++ src/lib/database/mysql.js
@@ -4,6 +4,7 @@ const dbConfig = {
   host: 'htl-projekt.com',
   user: 'ganihasmegaj',
   password: '!Insy_2024$',
+  port:33060,
   database: '2024_4bw_ganihasmegaj_albanianrivers',
   waitForConnections: true,
   connectionLimit: 10,
